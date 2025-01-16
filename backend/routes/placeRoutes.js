f7a504519d72e1cac2b7cb988a66d9238c4cd8c0
Apsara <aryal.apsara23@gmail.com>
2025-01-16 19:46:54 +0545

    Updated backend controllers and routes

--- backend/routes/placeRoutes.js
+++ backend/routes/placeRoutes.js
@@ -1,3 +1,34 @@
+// import express from "express";
+// import {
+//   getPlaces,
+//   getPlaceById,
+//   addPlace,
+//   updatePlace,
+//   deletePlace,
+//   searchPlaces,
+// } from "../controllers/placeController.js";
+
+// const router = express.Router();
+
+// // Get all places
+// router.get("/", getPlaces);
+
+// // Get a place by ID
+// router.get("/:id", getPlaceById);
+
+// // Add a new place
+// router.post("/", addPlace);
+
+// // Update an existing place
+// router.put("/:id", updatePlace);
+
+// // Delete a place
+// router.delete("/:id", deletePlace);
+
+// // Search places by name
+// router.get("/search/:query", searchPlaces);
+
+// export default router;
 import express from "express";
 import {
   getPlaces,
@@ -10,22 +41,12 @@ import {
 
 const router = express.Router();
 
-// Get all places
+// Routes for places
 router.get("/", getPlaces);
-
-// Get a place by ID
 router.get("/:id", getPlaceById);
-
-// Add a new place
 router.post("/", addPlace);
-
-// Update an existing place
 router.put("/:id", updatePlace);
-
-// Delete a place
 router.delete("/:id", deletePlace);
-
-// Search places by name
 router.get("/search/:query", searchPlaces);
 
 export default router;
