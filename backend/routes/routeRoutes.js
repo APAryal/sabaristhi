c78394f80a760a1f3601b92276397bd7d8db643f
Apsara <aryal.apsara23@gmail.com>
2025-01-16 19:53:35 +0545

    Resolved conflicts and staged changes

--- backend/routes/routeRoutes.js
+++ backend/routes/routeRoutes.js
@@ -1,3 +1,35 @@
+// import express from "express";
+// import {
+//   getRoutes,
+//   getRouteById,
+//   addRoute,
+//   updateRoute,
+//   deleteRoute,
+//   searchRoutesByPlace
+
+// } from "../controllers/routeController.js";
+
+// const router = express.Router();
+
+// // Get all routes
+// router.get("/", getRoutes);
+
+// // Get a route by ID
+// router.get("/:id", getRouteById);
+
+// // Add a new route
+// router.post("/", addRoute);
+
+// // Update an existing route
+// router.put("/:id", updateRoute);
+
+// // Delete a route
+// router.delete("/:id", deleteRoute);
+
+// router.get("/search/:query", searchRoutesByPlace);
+
+
+// export default router;
 import express from "express";
 import {
   getRoutes,
@@ -6,27 +38,17 @@ import {
   updateRoute,
   deleteRoute,
   searchRoutesByPlace
-
 } from "../controllers/routeController.js";
 
 const router = express.Router();
 
-// Get all routes
+// Routes for routes
 router.get("/", getRoutes);
-
-// Get a route by ID
 router.get("/:id", getRouteById);
-
-// Add a new route
 router.post("/", addRoute);
-
-// Update an existing route
 router.put("/:id", updateRoute);
-
-// Delete a route
 router.delete("/:id", deleteRoute);
-
 router.get("/search/:query", searchRoutesByPlace);
 
-
 export default router;
+
